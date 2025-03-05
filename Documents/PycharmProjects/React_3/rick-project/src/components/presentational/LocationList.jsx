b77142b423cd642b340981222607e1dccb44a2f1
GlavMech <165685784+GlavMech@users.noreply.github.com>
2025-03-05 14:32:51 +0500

    Update LocationList.jsx

--- Documents/PycharmProjects/React_3/rick-project/src/components/presentational/LocationList.jsx
+++ Documents/PycharmProjects/React_3/rick-project/src/components/presentational/LocationList.jsx
@@ -2,32 +2,46 @@ import { useState } from "react";
 import { Collapse } from "./Collapse";
 import { CharacterList } from "./CharacterList";
 import { useLocations } from "../hooks/useLocations";
+import { LocationItem } from "./LocationItem"; 
 
 export const LocationList = () => {
-  const { locations } = useLocations();
-  const [openLocations, setOpenLocations] = useState({});
+  const { locations } = useLocations(); 
+  const [openLocations, setOpenLocations] = useState({}); 
 
   const toggleLocation = (id) => {
     setOpenLocations((prev) => ({
       ...prev,
-      [id]: !prev[id],
+      [id]: !prev[id], 
     }));
   };
 
   return (
     <div>
       {locations.map((location) => (
-        <Collapse
-          key={location.id}
-          className="location"
-          title={location.name}
-          open={openLocations[location.id]}
-          setOpen={() => toggleLocation(location.id)}
-        >
-          <CharacterList
-            ids={location.residents.map((resident) => resident.split("/").pop())}
-          />
-        </Collapse>
+        <div key={location.id} className="location-item">
+          <button
+            onClick={() => toggleLocation(location.id)}
+            className="location-title"
+          >
+            {location.name} 
+          </button>
+
+          {openLocations[location.id] && (
+            <Collapse
+              key={location.id}
+              className="location"
+              title={location.name}
+              open={openLocations[location.id]}
+              setOpen={() => toggleLocation(location.id)}
+            >
+              <LocationItem location={location} />
+
+              <CharacterList
+                ids={location.residents.map((resident) => resident.split("/").pop())}
+              />
+            </Collapse>
+          )}
+        </div>
       ))}
     </div>
   );
