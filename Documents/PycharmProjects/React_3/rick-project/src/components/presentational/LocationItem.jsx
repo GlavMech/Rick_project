f03143a554817dad98ec7edce77dd902fc318c86
GlavMech <165685784+GlavMech@users.noreply.github.com>
2025-03-05 14:54:18 +0500

    Update LocationItem.jsx

--- Documents/PycharmProjects/React_3/rick-project/src/components/presentational/LocationItem.jsx
+++ Documents/PycharmProjects/React_3/rick-project/src/components/presentational/LocationItem.jsx
@@ -1,7 +1,6 @@
 export const LocationItem = ({ location }) => {
     return (
       <div className="location-item">
-        <h3>{location.name}</h3>
         <p>Тип: {location.type}</p>
         <p>Измерение: {location.dimension}</p>
         <p>Количество жителей: {location.residents.length}</p>
